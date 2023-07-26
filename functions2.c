d4b67e48da507c3c861bde0e1645f62ea91a561f
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-26 15:47:10 -0700

    commit

--- functions2.c
+++ functions2.c
@@ -30,3 +30,46 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+ * sub -subtracts the top element of the stack
+ * from the second top element of the stack.
+ * @stack: stack input
+ * @line_number: int input
+ * Return: void.
+ */
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * div_op - divides the second top element of the stack by
+ * the top element of the stack.
+ * @stack: stack input
+ * @line_number: int input
+ * Return: void.
+ */
+
+void div_op(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_number);
+}
