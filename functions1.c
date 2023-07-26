41e60de82c0962206513bf8c2004944300b2b474
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-26 14:57:05 -0700

    commit

--- functions1.c
+++ functions1.c
@@ -65,3 +65,25 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * pop - removes the top element of the stack.
+ * @stack: stack input
+ * @line_number: int input
+ * Return: void.
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
+}
