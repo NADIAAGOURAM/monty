561dd5b037ef22d007606b50c60d92d9c4c3f13c
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-26 14:42:43 -0700

    commit

--- functions1.c
+++ functions1.c
@@ -47,3 +47,20 @@ void pall(stack_t **stack, unsigned int line_number)
 		current = current->next;
 	}
 }
+
+/**
+ * pint - print the value of the top of the stack.
+ * @stack: stack input
+ * @line_number: int input
+ * Return: void.
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
