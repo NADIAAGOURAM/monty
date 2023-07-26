86098f719efa3f8d998ca817840b8d7f02483e35
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-07-26 15:27:41 -0700

    commit

--- functions1.c
+++ functions1.c
@@ -87,3 +87,56 @@ void pop(stack_t **stack, unsigned int line_number)
 		(*stack)->prev = NULL;
 	free(temp);
 }
+
+/**
+ * swap -  swaps the top two elements of the stack.
+ * @stack: stack input
+ * @line_number: int input
+ * Return: void.
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	(void)line_number;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "Error: can't swap, stack too short\n");
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
+
+/**
+ * add - adds the top two elements of the stack.
+ * @stack: stack input
+ * @line_number: int input
+ * Return: void.
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * nop -  doesn’t do anything.
+ * @stack: stack input
+ * @line_number: int input
+ * Return: void.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
